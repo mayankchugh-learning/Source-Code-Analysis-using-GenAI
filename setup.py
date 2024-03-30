c819515efa2d5d8c2bf9e068628746253971309e
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-03-30 13:50:39 +0800

    code dommit after added requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mayank Chugh',
+    author_email= 'mayankchugh.learning@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
